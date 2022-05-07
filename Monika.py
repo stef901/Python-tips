15b0062f4545a4c26adff1ad3389066ca4989f30
esthi <togom11@g.skku.edu>
2022-05-07 11:59:16 +0200

    Update Monika.py

--- Monika.py
+++ Monika.py
@@ -6,27 +6,48 @@ Sie die Rückgabewerte der Funktion in zwei Listen, um diese dann mit Hilfe der 
 auszugeben.
 Tipp: Wenn Sie nicht wissen, wie Sie in einer Funktion mehrere Werte zurückgeben können,
 betrachten Sie Beispiel 76 im Skript auf Seite 102.
+
+math
+
+
+def basic_operations():
+  PI = 3.14
+  r = float(input("Enter the radius of a circle:"))
+  area = PI * r * r
+  U = 2 * r * PI
+  return  PI, r, U, area
+  
+print("Area of a circle = %.2f" %basic_operations()[3])
+
+
 Aufgabe 2
 Schreiben Sie eine Funktion greet, welche bis zu drei Parameter akzeptiert, jedoch mindestens
 einen Parameter benötigt, nämlich den Namen einer Person. Unten finden Sie einige Beispiele, welche Ausgaben die Funktion generieren sollten. Passen Sie Ihre Funktion so an, dass die
 Beispiele in Tabelle 1 funktionieren. Falls Sie Hilfe brauchen, schauen Sie sich Beispiel 73 auf
 Seite 94 im Skript an.
+
+"""
+student_printer.py
+"""
+def greet(name, begrüssung=" Guten Morgen!", temp = 20):
+    print(name, begrüssung, "Es ist", temp, "Grad draussen.")
+
+
+greet("Monika")
+
+greet("Sepp", "Wie geht's denn so?") 
+
+greet("Hans", temp=10)
+
+
+
 Aufgabe 3
-Betrachten Sie das untenstehendes Programm. Sie werden bemerken, dass das Programm verschiedene Verantwortungen übernimmt. Ihre Aufgabe ist es, die einzelnen Verantwortungen
-zu identifizieren und für jede Verantwortung je eine Funktion zu definieren und den Code entsprechend auszulagern. Rufen Sie danach Ihre Funktionen in der richtigen Reihenfolge auf –
+Betrachten Sie das untenstehendes Programm. Sie werden bemerken, dass das Programm verschiedene Verantwortungen übernimmt. 
+Ihre Aufgabe ist es, die einzelnen Verantwortungen
+zu identifizieren und für jede Verantwortung je eine Funktion zu definieren und den Code entsprechend auszulagern.
+Rufen Sie danach Ihre Funktionen in der richtigen Reihenfolge auf –
 verwenden Sie wo sinnvoll Parameter und Rückgaben.
-
-
-Funktionsaufruf Ausgabe
-greet("Monika") Hallo Monika!
-Guten Morgen!
-Es ist 20 Grad draussen.
-greet("Sepp", "Wie geht's denn so?") Hallo Sepp!
-Wie geht's denn so?
-Es ist 20 Grad draussen.
-greet("Hans", temp=10) Hallo Hans!
-Guten Morgen!
-Es ist 10 Grad draussen.
+ 
 Tabelle 1: Ihre Funktion sollte dieselben Ausgaben generieren, wie in dieser Tabelle dargestellt.
 1 ages = []
 2 done = False
@@ -45,3 +66,20 @@ Tabelle 1: Ihre Funktion sollte dieselben Ausgaben generieren, wie in dieser Tab
 15
 16 import statistics
 17 print("Mittelwert der Alter: ", round(statistics.mean(ages), 2))
+
+
+
+Write a function that has a formal parameter corresponding to the radius of a circle. 
+
+The function should calculate the area and circumference of the circle and then return the calculations using the return keyword. 
+Call
+call the function 3 times, each time with a different actual parameter, and save the
+the return values of the function into two lists, and then output them using the function print
+function.
+Tip: If you don't know how to return multiple values in one function,
+consider Example 76 in the script on page 102.
+
+
+call  jh
+
+
