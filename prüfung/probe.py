d1d8655a3039f618be4174ef72dd81f571532cd6
esthi <togom11@g.skku.edu>
2022-06-02 15:56:39 +0200

    Create probe.py

---
+++ prüfung/probe.py
@@ -0,0 +1,6 @@
+message = 'Beurteilen Sie, ob folgende Aussagen jeweils wahr oder falsch sind'
+
+# führende und nachgestellte Leerzeichen entfernen
+print('Beurteilen Sie, ob folgende Aussagen jeweils wahr oder falsch sind:', message.strip())
+
+# Ausgabe: Nachricht: Python lernen
